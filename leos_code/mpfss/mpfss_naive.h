4bede2847c5995b5e21421cc7bf14983db000bea
Leonie Reichert <reicherl@hu-berlin.de>
2018-12-03 14:27:11 +0100

    All libararies added. Makefile still not working

--- leos_code/mpfss/mpfss_naive.h
+++ leos_code/mpfss/mpfss_naive.h
@@ -1,7 +1,18 @@
+//ensures header file is only included once
+#pragma once
 
 // initial amount for input data:
 #define ALLOC 128 
 // doubled when needed in load_data()
 
-void myPrintHelloMake(void);
 
+double lap;
+int cp;
+
+typedef struct {
+    int n; // number of data points
+    
+    int r; // seed
+} protocolIO;
+
+void mpfss_naive(void* args);
