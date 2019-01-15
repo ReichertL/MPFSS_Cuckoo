db3c162dac7d217d73cbec33c90af2df6489a119
Leonie Reichert <leonie.reichert@hu-berlin.de>
2019-01-15 13:21:05 +0100

    moved define of block_no to .oh file

---
+++ leos_code/mpfss/mpfss_naive.d
@@ -0,0 +1,7 @@
+mpfss_naive.o: ../mpfss/mpfss_naive.c \
+ ../../obliv-c/src/ext/oblivc/obliv.h \
+ ../../obliv-c/src/ext/oblivc/obliv_types.h \
+ ../../obliv-c/src/ext/oblivc/obliv_psi.h include/dbg.h \
+ /home/turing/semesterprojekt_code/leos_code/util/util.h \
+ /home/turing/semesterprojekt_code/leos_code/mpfss/mpfss_naive.h \
+ /home/turing/semesterprojekt_code/leos_code/mpfss/mpfss_naive.oh
