f34dfd8cd3c5d6ad7fc609f418104fc39a619fae
Leonie Reichert <leonie.reichert@hu-berlin.de>
2019-01-31 12:18:33 +0100

    Now case m>t accounted for. Also case batchlen<=1 accounted for. calculation of epsilon and s on the fly

--- leos_code/mpfss_batch_codes/graphs/analysis_n-variable.py
+++ leos_code/mpfss_batch_codes/graphs/analysis_n-variable.py
@@ -1,8 +1,12 @@
 from math import log
 import matplotlib.pyplot as plt
+from decimal import Decimal
 
 t=5000
-p=pow(10.,-5.)
+p_inverse=pow(10.,-25.)
+print(p_inverse)
+p=Decimal(1)-Decimal(p_inverse)
+print(p)
 
 def plot_this( x_value_exact, y_values_exact, name_xaxis, name_yaxis, title_addition, lable_addition, lable ):
 
@@ -36,7 +40,7 @@ def plot_this_loglog( x_value_exact, y_values_exact, name_xaxis, name_yaxis, tit
 
 
 def main():
-	n_list = [50000., 100000, 500000,1000000, 2000000, 3000000, 4000000,  5000000]
+	n_list = [10000. ,50000., 100000, 500000,1000000, 2000000, 3000000, 4000000,  5000000]
 	s_list=list()
 	d_list=list()
 	e_list=list()
@@ -44,55 +48,79 @@ def main():
 	rt_list=list()
 	rt_list_bst=list()
 	rt_list_ll=list()
-	rt_list_naive=[n*t for n in n_list]
+	rt_list_naive=[2*n*t for n in n_list]
 	t_to_n_list=[t/n for n in n_list]
 
 	for n in n_list:
 		s=log(n)/log(t)
 		s_list.append(s)
 
-		d=log(-p+1)/(-2*log(t))
-		d_list.append(d+1)
+		d=log(Decimal(1)-Decimal(p))/(-2*log(t))+1
+		d_list.append(d)
 
-		e=(d)/(s+1)
+		e=(d-1)/(s+1)
 		e_list.append(e)
 
 		m=pow(t, 1+e)
 		m_list.append(m)
 
-		rt= t*n*(d+1)/m+m*m+t*d
+		rt= t*n*d/m+n*d+t*d
 		rt_list.append(rt)
 
-
-		rt_bst= t*n*(d+1)/m+m*m+t*m/2*((n*d)/m)
+		rt_bst= t*n*d/m+n*d+t*m/2*((n*d)/m)
 		rt_list_bst.append(rt_bst)
 
-		rt_ll= t*n*(d+1)/m+m*m+t*n*m/2
+		rt_ll= t*n*d/m+n*d+t*n*m/2
 		rt_list_ll.append(rt_ll)
 
 
-
 	plt.figure()
-	plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt ", " t: "+str(t)+" p: "+str(p), "Batch Codes", "o-" )
-	plot_this( t_to_n_list, rt_list_naive, "Quotient t/n", "Runtime rt ", " t: "+str(t)+" p: "+str(p), "Naive", "x--" )
-	filename="Analysis-x:t_to_n-y:rt-p:"+str(p)+"-t:"+str(t)
+	plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt ", " t: "+str(t)+" p: 1-"+str(p_inverse), "Batch Codes", "o-" )
+	plot_this( t_to_n_list, rt_list_naive, "Quotient t/n", "Runtime rt ", " t: "+str(t)+" p: 1-"+str(p_inverse), "Naive", "x--" )
+	filename="Analysis-x:t_to_n-y:rt-p:1-"+str(p_inverse)+"-t:"+str(t)
 	art = []
 	lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
 	art.append(lgd)
-	plt.savefig("runtime/"+filename+".png", additional_artists=art, bbox_inches="tight")
+	plt.savefig("runtime/n_variable/"+filename+".png", additional_artists=art, bbox_inches="tight")
 	plt.close()
 
-
 	plt.figure()
-	plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt", " t: "+str(t)+" p: "+str(p), "Batch Codes", "bo-" )
-	plot_this( t_to_n_list, rt_list_bst, "Quotient t/n", "Runtime rt", " t: "+str(t)+" p: "+str(p), "Batch Codes(BST)", "ro-" )
-
-
-	filename="Analysis-x:t_to_n-y:rt-p:"+str(p)+"-n:"+str(n)+"-comparison-to-bst"
+	plot_this( n_list, rt_list, "Size of Inputfield n ", "Runtime rt ", " t: "+str(t)+" p: 1-"+str(p_inverse), "Batch Codes", "o-" )
+	plot_this( n_list, rt_list_naive, "Size of Inputfield ", "Runtime rt ", " t: "+str(t)+" p: 1-"+str(p_inverse), "Naive", "x--" )
+	filename="Analysis-x:n-y:rt-p:1-"+str(p_inverse)+"-t:"+str(t)
 	art = []
 	lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
 	art.append(lgd)
-	plt.savefig("runtime/"+filename+".png", additional_artists=art, bbox_inches="tight")
+	plt.savefig("runtime/n_variable/"+filename+".png", additional_artists=art, bbox_inches="tight")
 	plt.close()
+
+	plt.figure()
+	plot_this( n_list, m_list, "Size of Inputfield n ", "Number of Batches m ", " t: "+str(t)+" p: 1-"+str(p_inverse), "", "o-" )
+	filename="Analysis-x:n-y:m-p:1-"+str(p_inverse)+"-t:"+str(t)
+	art = []
+	lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+	art.append(lgd)
+	plt.savefig("m/"+filename+".png", additional_artists=art, bbox_inches="tight")
+	plt.close()
+
+	plt.figure()
+	plot_this( n_list, d_list, "Size of Inputfield n ", "Repetition Factor d ", " t: "+str(t)+" p: 1-"+str(p_inverse), "", "o-" )
+	filename="Analysis-x:n-y:d-p:1-"+str(p_inverse)+"-t:"+str(t)
+	art = []
+	lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+	art.append(lgd)
+	plt.savefig("d/"+filename+".png", additional_artists=art, bbox_inches="tight")
+	plt.close()
+
+
+	#plt.figure()
+	#plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt", " t: "+str(t)+" p: 1-"+str(p_inverse), "Batch Codes", "bo-" )
+	#plot_this( t_to_n_list, rt_list_bst, "Quotient t/n", "Runtime rt", " t: "+str(t)+" p: 1-"+str(p_inverse), "Batch Codes(BST)", "ro-" )
+	#filename="Analysis-x:t_to_n-y:rt-p:1-"+str(p_inverse)+"-n:"+str(n)+"-comparison-to-bst"
+	#art = []
+	#lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+	#art.append(lgd)
+	#plt.savefig("runtime/n_variable/"+filename+".png", additional_artists=art, bbox_inches="tight")
+	#plt.close()
 
 main()
--- leos_code/mpfss_batch_codes/graphs/analysis_t-variable.py
+++ leos_code/mpfss_batch_codes/graphs/analysis_t-variable.py
@@ -1,8 +1,12 @@
 from math import log
 import matplotlib.pyplot as plt
+from decimal import Decimal
 
-n=1000000.
-p=pow(10.,-5.)
+
+p_inverse=pow(10.,-25.)
+print(p_inverse)
+p=Decimal(1)-Decimal(p_inverse)
+print(p)
 
 def plot_this( x_value_exact, y_values_exact, name_xaxis, name_yaxis, title_addition, lable_addition, lable ):
 
@@ -36,80 +40,139 @@ def plot_this_loglog( x_value_exact, y_values_exact, name_xaxis, name_yaxis, tit
 
 
 def main():
-	t_list = [ 50., 500., 1000., 2000., 3000., 4000., 5000., 10000., 20000., 30000.,40000., 50000., 100000., 200000., 300000.,500000.]
-	s_list=list()
-	d_list=list()
-	e_list=list()
-	m_list=list()
-	rt_list=list()
-	rt_list_bst=list()
-	rt_list_ll=list()
-	rt_list_naive=[n*n*t for t in t_list]
-	t_to_n_list=[t/n for t in t_list]
+	n_list=[1000000.]
+	for n in n_list:
+		t_list = [ 100., 200., 400., 800., 1600.,5000.]
+		s_list=list()
+		d_list=list()
+		e_list=list()
+		m_list=list()
+		rt_list=list()
+		rt_list_bst=list()
+		rt_list_ll=list()
+		rt_list_naive=[2*n*t for t in t_list]
+		t_to_n_list=[t/n for t in t_list]
 
-	for t in t_list:
-		s=log(n)/log(t)
-		s_list.append(s)
+		for t in t_list:
+			s=log(n)/log(t)
+			s_list.append(s)
 
-		d=log(-p+1)/(-2*log(t))
-		d_list.append(d+1)
+			d=log(Decimal(1)-Decimal(p))/(-2*log(t))+1
+			d_list.append(d)
 
-		e=(d)/(s+1)
-		e_list.append(e)
+			e=(d-1)/(s+1)
+			e_list.append(e)
 
-		m=pow(t, 1+e)
-		m_list.append(m)
+			m=pow(t, 1+e)
+			m_list.append(m)
 
-		rt= t*n*(d+1)/m+n*d+t*d
-		rt_list.append(rt)
+			rt= t*n*d/m+n*d+t*d
+			rt_list.append(rt)
 
-		rt_bst= t*n*(d+1)/m+n*d+t*m/2*((n*d)/m)
-		rt_list_bst.append(rt_bst)
+			#print("s:"+str(s)+" d:"+str(d)+" e:"+str(e)+" m:"+str(m)+" rt:"+str(rt))
+			print(" m:"+str(m/ pow(10,6)))
+#			print(" rt:"+str(rt / pow(10, 7)))
+			rt_bst= t*n*d/m+n*d+t*m/2*((n*d)/m)
+			rt_list_bst.append(rt_bst)
 
-		rt_ll= t*n*(d+1)/m+n*d+t*n*m/2
-		rt_list_ll.append(rt_ll)
-
-
-	plt.figure()
-	plot_this( t_to_n_list, e_list, "Quotient t/n", "Epsilon e ", " n: "+str(n)+" p: "+str(p), "", "o-" )
-	filename="Analysis-x:t_to_n-y:e-p:"+str(p)+"-n:"+str(n)
-	art = []
-	lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
-	art.append(lgd)
-	plt.savefig("e/"+filename+".png", additional_artists=art, bbox_inches="tight")
-	plt.close()
-
-	plt.figure()
-	plot_this( t_to_n_list, d_list, "Quotient t/n", "Repetition Factor d ", " n: "+str(n)+" p: "+str(p), "", "o-" )
-	filename="Analysis-x:t_to_n-y:d-p:"+str(p)+"-n:"+str(n)
-	art = []
-	lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
-	art.append(lgd)
-	plt.savefig("d/"+filename+".png", additional_artists=art, bbox_inches="tight")
-	plt.close()
+			rt_ll= t*n*d/m+n*d+t*n*m/2
+			rt_list_ll.append(rt_ll)
 
 
-	plt.figure()
-	plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt ", " n: "+str(n)+" p: "+str(p), "Batch Codes", "o-" )
-	plot_this( t_to_n_list, rt_list_naive, "Quotient t/n", "Runtime rt ", " n: "+str(n)+" p: "+str(p), "Naive", "x--" )
-	filename="Analysis-x:t_to_n-y:rt-p:"+str(p)+"-n:"+str(n)
-	art = []
-	lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
-	art.append(lgd)
-	plt.savefig("runtime/"+filename+".png", additional_artists=art, bbox_inches="tight")
-	plt.close()
+		plt.figure()
+		plot_this( t_to_n_list, e_list, "Quotient t/n", "Epsilon e ", " n: "+str(n)+" p: 1-"+str(p_inverse), "", "o-" )
+		filename="Analysis-x:t_to_n-y:e-p:1-"+str(p_inverse)+"-n:"+str(n)
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("e/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
+
+		plt.figure()
+		plot_this( t_list, e_list, "Number Points of MPF t ", "Epsilon e ", " n: "+str(n)+" p: 1-"+str(p_inverse), "", "o-" )
+		filename="Analysis-x:t-y:e-p:1-"+str(p_inverse)+"-n:"+str(n)
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("e/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
+
+		plt.figure()
+		plot_this( t_to_n_list, d_list, "Quotient t/n", "Repetition Factor d ", " n: "+str(n)+" p: 1-"+str(p_inverse), "", "o-" )
+		filename="Analysis-x:t_to_n-y:d-p:1-"+str(p_inverse)+"-n:"+str(n)
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("d/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
 
 
-	plt.figure()
-	plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt", " n: "+str(n)+" p: "+str(p), "Batch Codes", "bo-" )
-	plot_this( t_to_n_list, rt_list_bst, "Quotient t/n", "Runtime rt", " n: "+str(n)+" p: "+str(p), "Batch Codes(BST)", "ro-" )
+		plt.figure()
+		plot_this( t_list, d_list, "Number Points of MPF t", "Repetition Factor d ", " n: "+str(n)+" p: 1-"+str(p_inverse), "", "o-" )
+		filename="Analysis-x:t-y:d-p:1-"+str(p_inverse)+"-n:"+str(n)
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("d/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
+
+		plt.figure()
+		plot_this( t_list, m_list, "Number Points of MPF t", "Number of Batches m ", " n: "+str(n)+" p: 1-"+str(p_inverse), "", "o-" )
+		filename="Analysis-x:t-y:m-p:1-"+str(p_inverse)+"-n:"+str(n)
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("m/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
+
+		plt.figure()
+		plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt ", " n: "+str(n)+" p: 1-"+str(p_inverse), "Batch Codes", "o-" )
+		plot_this( t_to_n_list, rt_list_naive, "Quotient t/n", "Runtime rt ", " n: "+str(n)+" p: 1-"+str(p_inverse), "Naive", "x--" )
+		filename="Analysis-x:t_to_n-y:rt-p:1-"+str(p_inverse)+"-n:"+str(n)
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("runtime/t_variable/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
 
 
-	filename="Analysis-x:t_to_n-y:rt-p:"+str(p)+"-n:"+str(n)+"-comparison-to-bst"
-	art = []
-	lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
-	art.append(lgd)
-	plt.savefig("runtime/"+filename+".png", additional_artists=art, bbox_inches="tight")
-	plt.close()
+		plt.figure()
+		plot_this( t_list, rt_list, "Number Points of MPF t ", "Runtime rt ", " n: "+str(n)+" p: 1-"+str(p_inverse), "Batch Codes", "o-" )
+		plot_this( t_list, rt_list_naive, "Number Points of MPF t ", "Runtime rt ", " n: "+str(n)+" p: 1-"+str(p_inverse), "Naive", "x--" )
+		filename="Analysis-x:t-y:rt-p:1-"+str(p_inverse)+"-n:"+str(n)
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("runtime/t_variable/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
+
+		#plt.figure()
+		#plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt", " n: "+str(n)+" p: 1-"+str(p_inverse), "Batch Codes", "bo-" )
+		#plot_this( t_to_n_list, rt_list_bst, "Quotient t/n", "Runtime rt", " n: "+str(n)+" p: 1-"+str(p_inverse), "Batch Codes(BST)", "ro-" )
+		#filename="Analysis-x:t_to_n-y:rt-p:1-"+str(p_inverse)+"-n:"+str(n)+"-comparison-to-bst"
+		#art = []
+		#lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		#art.append(lgd)
+		#plt.savefig("runtime/t_variable/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		#plt.close()
+
+		plt.figure()
+		plot_this( t_to_n_list, rt_list, "Quotient t/n", "Runtime rt", " n: "+str(n)+" p: 1-"+str(p_inverse), "Batch Codes", "bo-" )
+		filename="Analysis-x:t_to_n-y:rt-p:1-"+str(p_inverse)+"-n:"+str(n)+"-single"
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("runtime/t_variable/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
+
+
+		plt.figure()
+		plot_this( t_list, rt_list, "Number Points of MPF t ", "Runtime rt", " n: "+str(n)+" p: 1-"+str(p_inverse), "Batch Codes", "bo-" )
+		filename="Analysis-x:t-y:rt-p:1-"+str(p_inverse)+"-n:"+str(n)+"-single"
+		art = []
+		lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+		art.append(lgd)
+		plt.savefig("runtime/t_variable/"+filename+".png", additional_artists=art, bbox_inches="tight")
+		plt.close()
 
 main()
