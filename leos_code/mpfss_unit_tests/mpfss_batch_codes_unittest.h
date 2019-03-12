1eff4d655f94b7b60d7493bc4258fa02dcb92b4d
Leonie Reichert <leonie.reichert@hu-berlin.de>
2019-03-12 19:49:29 +0100

    cleaning up repo

--- leos_code/mpfss_unit_tests/mpfss_batch_codes_unittest.h
+++
@@ -1,6 +0,0 @@
-//ensures header file is only included once
-#pragma once
-
-
-
-void TEST_ALL_mpfss_batch_codes(bool *err);
--- leos_code/mpfss_unit_tests/mpfss_naive_unittest.h
+++
@@ -1,8 +0,0 @@
-//ensures header file is only included once
-#pragma once
-
-
-bool TEST_get_mpfss_vectors(int t, size_t size);
-bool TEST_new_mpfss_naive( int t, size_t size);
-bool TEST_dpf(size_t size, int index);
-void TEST_ALL_mpfss_naive(bool *err);
